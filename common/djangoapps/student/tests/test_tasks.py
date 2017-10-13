9e44b1549264730bece7da80f730d2de1ad0e615
uzairr <uzairr@yahoo.com>
2017-10-13 12:54:55 +0500

    Celery task to update sailthru purchase record
    After change in the audit enrollment process, edX platform is
    no longer keeping its audit enrollment record on sailthru.To keep
    updated sailthru a celery task is created that will update sailthru
    user profile in case of enroll/un-enroll of any audit enrollment course.
    
    LEARNER-2694

--- common/djangoapps/student/tests/test_tasks.py
+++ common/djangoapps/student/tests/test_tasks.py
@@ -1,20 +1,24 @@
 """
 Tests for the Sending activation email celery tasks
 """
-
+import unittest
 import mock
 from boto.exception import NoAuthHandlerFound
 from django.conf import settings
 from django.test import TestCase
-
+from student.tests.factories import CourseEnrollmentFactory
 from lms.djangoapps.courseware.tests.factories import UserFactory
-from student.tasks import send_activation_email
+from opaque_keys.edx.keys import CourseKey
+from student.tasks import send_activation_email, update_course_enrollment, update_sailthru
+
+TEST_EMAIL = "test@edx.org"
 
 
 class SendActivationEmailTestCase(TestCase):
     """
     Test for send activation email to user
     """
+
     def setUp(self):
         """ Setup components used by each test."""
         super(SendActivationEmailTestCase, self).setUp()
@@ -51,3 +55,97 @@ class SendActivationEmailTestCase(TestCase):
             exc_info=True
         )
         self.assertEquals(mock_log.error.call_count, 1)
+
+
+class MockSailthruResponse(object):
+    """
+    Mock object for SailthruResponse
+    """
+
+    def __init__(self, json_response, error=None, code=1):
+        self.json = json_response
+        self.error = error
+        self.code = code
+
+    def is_ok(self):
+        """
+        Return true of no error
+        """
+        return self.error is None
+
+    def get_error(self):
+        """
+        Get error description
+        """
+        return MockSailthruError(self.error, self.code)
+
+
+class MockSailthruError(object):
+    """
+    Mock object for Sailthru Error
+    """
+
+    def __init__(self, error, code=1):
+        self.error = error
+        self.code = code
+
+    def get_message(self):
+        """
+        Get error description
+        """
+        return self.error
+
+    def get_error_code(self):
+        """
+        Get error code
+        """
+        return self.code
+
+
+class SailthruTests(TestCase):
+    """
+    Tests for the Sailthru tasks class.
+    """
+
+    def setUp(self):
+        super(SailthruTests, self).setUp()
+        self.user = UserFactory()
+        self.course_id = CourseKey.from_string('edX/toy/2012_Fall')
+        self.course_url = 'http://lms.testserver.fake/courses/edX/toy/2012_Fall/info'
+        self.course_id2 = 'edX/toy/2016_Fall'
+        self.course_url2 = 'http://lms.testserver.fake/courses/edX/toy/2016_Fall/info'
+
+    @unittest.skipUnless(settings.ROOT_URLCONF == 'lms.urls', 'Enrollment only supported in LMS')
+    @mock.patch('sailthru.sailthru_client.SailthruClient.purchase')
+    @mock.patch('sailthru.sailthru_client.SailthruClient.api_get')
+    @mock.patch('sailthru.sailthru_client.SailthruClient.api_post')
+    def test_update_course_enrollment(self, mock_sailthru_api_post,
+                                      mock_sailthru_api_get, mock_sailthru_purchase):
+        """test update sailthru user record"""
+
+        # create mocked Sailthru API responses
+        mock_sailthru_api_post.return_value = MockSailthruResponse({'ok': True})
+        mock_sailthru_api_get.return_value = MockSailthruResponse({'user': {"id": TEST_EMAIL, "fields": {"vars": 1}}})
+        mock_sailthru_purchase.return_value = MockSailthruResponse({'ok': True})
+        self.user.email = TEST_EMAIL
+        CourseEnrollmentFactory(user=self.user, course_id=self.course_id)
+        with mock.patch('student.tasks.build_course_url') as m:
+            m.return_value = self.course_url
+            update_course_enrollment(TEST_EMAIL, self.course_id, 'audit')
+        item = [{
+            'url': self.course_url,
+            'price': 0,
+            'qty': 1,
+            'id': 'edX/toy/2012_Fall-audit',
+            'title': 'Course edX/toy/2012_Fall mode: audit'
+        }]
+        mock_sailthru_purchase.assert_called_with(TEST_EMAIL, item, options={})
+
+    @mock.patch('sailthru.sailthru_client.SailthruClient.purchase')
+    def test_verify_dependency_on_flag(self, mock_sailthru_purchase):
+        update_sailthru(None, None, self.user, 'verified', self.course_id)
+        self.assertFalse(mock_sailthru_purchase.called)
+        with mock.patch('openedx.core.djangoapps.waffle_utils.WaffleSwitchNamespace.is_enabled') as flag:
+            flag.return_value = True
+            update_sailthru(None, None, self.user, 'verified', self.course_id)
+            self.assertFalse(mock_sailthru_purchase.called)
